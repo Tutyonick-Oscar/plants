9c25beb468e32ab402c1257ca981fded8931afbb
Tutyonick-Oscar <tutyonickoscar367@gmail.com>
2024-11-16 15:04:48 +0200

    path fixing

--- plants/routes/api_urls.py
+++ plants/routes/api_urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path,include
 from plants.usable import app_path
 
 urlpatterns = [
-    path('plants/',include(f'{app_path('plants_species')}.urls')),
-    path('accounts/',include(f'{app_path('user_app')}.urls.api')),
-    path('fields/',include(f'{app_path('field')}.urls.api'))
+    path("plants/",include(f"{app_path('plants_species')}.urls")),
+    path("accounts/",include(f"{app_path('user_app')}.urls.api")),
+    path("fields/",include(f"{app_path('field')}.urls.api"))
 ]
