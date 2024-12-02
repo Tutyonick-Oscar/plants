e8b1cfc6588ab9379aa6c19db894b885b233906d
Tutyonick-Oscar <tutyonickoscar367@gmail.com>
2024-12-02 11:40:01 +0200

    r.txt

--- plants/apps/plants_species/urls.py
+++ plants/apps/plants_species/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path,re_path,include
 from .views import PlantsSpecies,PlantsDetails,PlantDiseaseListe,PlantsCare,plantFaq
-from plants.settings.settings import PERENUAL_API_KEY
 
 urlpatterns = [
     path('plants-species/',PlantsSpecies.as_view(),name="plants-species"),
