58d7c4f86ddc7a6fe9d7a2f70bbf6becedcb76cc
Tutyonick-Oscar <tutyonickoscar367@gmsail.com>
2024-12-03 07:55:20 +0200

    rate limit handler

--- plants/apps/core/views.py
+++ plants/apps/core/views.py
@@ -1,10 +1,11 @@
 from rest_framework.generics import GenericAPIView
 from plants.apps.core.models import RequestOffset
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
-class InitRequestOffset(GenericAPIView):
-    def post(self,request):
-        offset = RequestOffset()
-        offset.save()
-        return Response({'success' : f'offset initialized to {offset.offset} successfuly !'})
+@api_view(['POST'])
+def init_request_offset(self,request):
+    offset = RequestOffset()
+    offset.save()
+    return Response({'success' : f'offset initialized to {offset.offset} successfuly !'})
--- plants/apps/user_app/urls/api.py
+++ plants/apps/user_app/urls/api.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from ..views import UsersViewSet
 from rest_framework import routers
 from rest_framework.authtoken import views
+from plants.apps.core.views import init_request_offset
 
 app_name = "user_app"
 router = routers.DefaultRouter()
@@ -10,4 +11,5 @@ router.register(r'accounts',UsersViewSet,basename='users')
 urlpatterns =[
     path('',include(router.urls)),
     path('login/',views.obtain_auth_token,name='api-login'),
+    path("init-offset/",init_request_offset,name="init-offset-request")
 ]
