c86dd7f4bb33d93a281c450d2dac42aa85737aa3
Tutyonick-Oscar <tutyonickoscar367@gmsail.com>
2024-12-03 08:07:41 +0200

    rate limit handler

--- plants/apps/core/views.py
+++ plants/apps/core/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 
 
 @api_view(['POST'])
-def init_request_offset(self,request):
+def init_request_offset(request):
     offset = RequestOffset()
     offset.save()
     return Response({'success' : f'offset initialized to {offset.offset} successfuly !'})
