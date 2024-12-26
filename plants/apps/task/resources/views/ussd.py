e6a62b892b5b87ce8a796ea4e0c1e9992b0030dd
Tutyonick-Oscar <tutyonickoscar367@gmsail.com>
2024-12-26 23:43:02 +0200

    request content type

--- plants/apps/task/resources/views/ussd.py
+++ plants/apps/task/resources/views/ussd.py
@@ -1,19 +1,25 @@
 from plants.apps.user_app.models import CustomUser
 from rest_framework.views import APIView
 from django.http  import HttpResponse
+from rest_framework.response import Response
 
 class GenerateUserIdent(APIView):
     
     def post(self,request):
-        session_id = request.values.get("sessionId", None)
-        service_code = request.values.get("serviceCode", None)
-        phone_number = request.values.get("phoneNumber", None)
-        text = request.values.get("text", "default")
         
-        if text == '':
-            response  = HttpResponse("CON Generate user ident ? \n1.Yes \n2.No")
+        content_type = request.META.get('CONTENT_TYPE','')
+        
+        if 'application/x-www-form-urlencoded' in content_type:
             
-        return response
+            session_id = request.values.get("sessionId", None)
+            service_code = request.values.get("serviceCode", None)
+            phone_number = request.values.get("phoneNumber", None)
+            text = request.values.get("text", "default")
+            
+            if text == '':
+                response  = Response("CON Generate user ident ? \n1.Yes \n2.No",content_type='text/plain')
+                
+            return response
     
     # user = CustomUser.objects.get(username = name)
     # user_ident = '000'+str(user.pk)
