ed26425754aa021bdcd6148131c557b35b1158ea
Tutyonick-Oscar <tutyonickoscar367@gmsail.com>
2024-12-27 01:04:11 +0200

    request content type

---
+++ plants/apps/core/renderers.py
@@ -0,0 +1,9 @@
+from rest_framework import renderers
+
+class PlainTextRenderer(renderers.BaseRenderer):
+    media_type = 'text/plain'
+    format = 'txt'
+    charset = 'iso-8859-1'
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        return data.encode(self.charset)
--- plants/apps/task/resources/views/ussd.py
+++ plants/apps/task/resources/views/ussd.py
@@ -2,23 +2,27 @@ from plants.apps.user_app.models import CustomUser
 from rest_framework.views import APIView
 from django.http  import HttpResponse
 from rest_framework.response import Response
+from plants.apps.core.renderers import PlainTextRenderer
 
 class GenerateUserIdent(APIView):
+    
+    renderer_classes = [PlainTextRenderer]
     
     def post(self,request):
         
         if request.content_type == 'application/x-www-form-urlencoded':
             
-            session_id = request.values.get("sessionId", None)
-            service_code = request.values.get("serviceCode", None)
-            phone_number = request.values.get("phoneNumber", None)
-            text = request.values.get("text", "default")
+            session_id = request.POST.get("sessionId")
+            service_code = request.POST.get("serviceCode")
+            phone_number = request.POST.get("phoneNumber")
+            text = request.POST.get("text")
             
             if text == '':
-                response  = Response("CON Generate user ident ? \n1.Yes \n2.No",content_type='text/plain')
-                
-            return response
-    
+                response  = Response(data="CON Generate user ident ? \n1.Yes \n2.No")
+                return response
+            
+        else:
+            return Response({'message' : 'wrong data format'})
     # user = CustomUser.objects.get(username = name)
     # user_ident = '000'+str(user.pk)
     # return HttpResponse(user_ident)
